a3fce23b049c4f590d2b829d173a1e8550efa28b
Sukhada11 <sukhadacoolkarni@gmail.com>
2022-09-09 14:30:34 -0700

    added the spawning mechanism and characters

--- Assets/scripts/NewBehaviourScript.cs
+++ Assets/scripts/NewBehaviourScript.cs
@@ -15,11 +15,6 @@ public class NewBehaviourScript : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
-    private void Start()
-    {
-        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
-    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
@@ -30,16 +25,5 @@ public class NewBehaviourScript : MonoBehaviour
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
         
-    }
-
-    private void AnimateSprite()
-    {
-        spriteIndex++;
-        if (spriteIndex >= sprites.Length)
-        {
-            spriteIndex = 0;
-        }
-
-        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
--- Assets/scripts/Spawner.cs
+++ Assets/scripts/Spawner.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -7,7 +5,7 @@ public class Spawner : MonoBehaviour
     public GameObject prefab;
     public float spawnRate = 1f;
     public float minHeight = -1f;
-    public float maxHeight = 1f;
+    public float maxHeight = 2f;
 
     private void OnEnable()
     {
@@ -18,10 +16,11 @@ public class Spawner : MonoBehaviour
     {
         CancelInvoke(nameof(Spawn));
     }
-    
+
     private void Spawn()
     {
         GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
         pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
     }
+
 }
